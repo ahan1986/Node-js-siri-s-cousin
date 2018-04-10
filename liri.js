ddcf87ac39b1fb18f501ed84d9756b1477c1961c
Handrew <andrew.a.han@gmail.com>
2018-04-10 18:04:40 -0400

    completed the omdb portion but haven't cleaned it up

--- liri.js
+++ liri.js
@@ -1,12 +1,134 @@
-var keys = require('./keys.js');
-// require("dotenv").config();
+require("dotenv").config();
+var keys = require('./keys');
+
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+
+var Twitter = require('twitter');
+var client = new Twitter(keys.twitter);
+
+var omdbApi = require('omdb-client');
+
+var fs = require('fs');
 
 var command = process.argv[2];
 
 // creating for loop to collect any words after the 3rd index
 var name = process.argv[3];
 
-for (var i=4; i<process.argv.length; i++) {
+for (var i = 4; i < process.argv.length; i++) {
     name += " " + process.argv[i];
 }
-console.log(name);
+
+switch (command) {
+    //tweeter portion
+    case 'my-tweets':
+    //using 'get' using the variable client that I have defined above
+    client.get('favorites/list', function(error, tweets, response) {
+        if(error) throw error;
+        console.log(tweets);  // The favorites. 
+        console.log(response);  // Raw response object. 
+      });
+    //using 'post'
+        // client.post('statuses/update', {status: name}, function(error, tweet, reponse) {
+        //     if(error) throw error;
+        //     console.log(tweet);
+        //     // console.log(response);
+        // })
+        break;
+
+    // spotify portion
+    case 'spotify-this-song':
+    //using variable spotify that i defined above
+        if (name !== undefined) {
+            spotify.search({ type: 'album', query: name }, function (err, data) {
+                if (err) {
+                    console.log("there was an error somewhere with the spotify " + err);
+                }
+                var spot = data.albums.items;
+                for (var i = 0; i < spot.length; i++) {
+                    var artists = spot[i].artists[0].name;
+                    var title = spot[i].name;
+                    var urlSpot = spot[i].external_urls.spotify;
+                    var album = spot[i].album_type;
+                    //showing the index along with the artists
+                    console.log(`${i + 1}: \n Artists: ${artists} \n Title: ${title} \n Preview Link: ${urlSpot} \n Album Type: ${album} \n`);
+                    console.log("===================================== \n");
+                }
+            })
+        } else {
+            spotify.search({ type: 'album', query: 'Ace of Base' }, function (err, data) {
+                if (err) {
+                    console.log("there was an error somewhere with the spotify " + err);
+                }
+                var spot = data.albums.items;
+                for (var i = 0; i < spot.length; i++) {
+                    var artists = spot[i].artists[0].name;
+                    var title = spot[i].name;
+                    var urlSpot = spot[i].external_urls.spotify;
+                    var album = spot[i].album_type;
+                    //showing the index along with the artists
+                    console.log(`${i + 1}: \n Artists: ${artists} \n Title: ${title} \n Preview Link: ${urlSpot} \n Album Type: ${album} \n`);
+                    console.log("===================================== \n");
+                }
+            })
+        }
+        break;
+
+    case 'movie-this':
+if(name !== undefined) {
+var params = {
+    apiKey: 'trilogy',
+    title: name
+}
+omdbApi.get(params, function(err, data) {
+    if(err) throw err;
+    // console.log(data);
+    //title
+    console.log(data.Title);
+    //year
+    console.log(data.Year);
+    //rotten tomatoes ratings
+    console.log(data.Ratings[1].Source + ": " + data.Ratings[1].Value);
+    //country
+    console.log(data.Country);
+    // language of the movie
+    console.log(data.Language);
+    //plot
+    console.log(data.Plot);
+    //actors in the movie
+    console.log(data.Actors);
+});
+} else {
+    var params = {
+        apiKey: 'trilogy',
+        title: 'Mr. Nobody'
+    }
+    omdbApi.get(params, function(err, data) {
+        if(err) throw err;
+        // console.log(data);
+        //title
+        console.log(data.Title);
+        //year
+        console.log(data.Year);
+        //rotten tomatoes ratings
+        console.log(data.Ratings[1].Source + ": " + data.Ratings[1].Value);
+        //country
+        console.log(data.Country);
+        // language of the movie
+        console.log(data.Language);
+        //plot
+        console.log(data.Plot);
+        //actors in the movie
+        console.log(data.Actors);
+    });
+}
+        break;
+    case 'do-what-it-says':
+    fs.readFile('random.txt','utf8', function(error, data) {
+        if(error) throw error;
+        var a = data.split(',');
+        console.log(a);
+    })
+        break;
+}
