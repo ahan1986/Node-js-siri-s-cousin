1b8094eddeacfb7fee55338a2210ec7023fd0df5
Handrew <andrew.a.han@gmail.com>
2018-04-10 18:33:24 -0400

    finished all the steps But have not cleaned up responses

--- liri.js
+++ liri.js
@@ -19,116 +19,127 @@ var name = process.argv[3];
 for (var i = 4; i < process.argv.length; i++) {
     name += " " + process.argv[i];
 }
-
-switch (command) {
-    //tweeter portion
-    case 'my-tweets':
-    //using 'get' using the variable client that I have defined above
-    client.get('favorites/list', function(error, tweets, response) {
-        if(error) throw error;
-        console.log(tweets);  // The favorites. 
-        console.log(response);  // Raw response object. 
-      });
-    //using 'post'
-        // client.post('statuses/update', {status: name}, function(error, tweet, reponse) {
-        //     if(error) throw error;
-        //     console.log(tweet);
-        //     // console.log(response);
-        // })
-        break;
-
-    // spotify portion
-    case 'spotify-this-song':
-    //using variable spotify that i defined above
-        if (name !== undefined) {
-            spotify.search({ type: 'album', query: name }, function (err, data) {
-                if (err) {
-                    console.log("there was an error somewhere with the spotify " + err);
-                }
-                var spot = data.albums.items;
-                for (var i = 0; i < spot.length; i++) {
-                    var artists = spot[i].artists[0].name;
-                    var title = spot[i].name;
-                    var urlSpot = spot[i].external_urls.spotify;
-                    var album = spot[i].album_type;
-                    //showing the index along with the artists
-                    console.log(`${i + 1}: \n Artists: ${artists} \n Title: ${title} \n Preview Link: ${urlSpot} \n Album Type: ${album} \n`);
-                    console.log("===================================== \n");
-                }
-            })
-        } else {
-            spotify.search({ type: 'album', query: 'Ace of Base' }, function (err, data) {
-                if (err) {
-                    console.log("there was an error somewhere with the spotify " + err);
-                }
-                var spot = data.albums.items;
-                for (var i = 0; i < spot.length; i++) {
-                    var artists = spot[i].artists[0].name;
-                    var title = spot[i].name;
-                    var urlSpot = spot[i].external_urls.spotify;
-                    var album = spot[i].album_type;
-                    //showing the index along with the artists
-                    console.log(`${i + 1}: \n Artists: ${artists} \n Title: ${title} \n Preview Link: ${urlSpot} \n Album Type: ${album} \n`);
-                    console.log("===================================== \n");
-                }
-            })
-        }
-        break;
-
-    case 'movie-this':
-if(name !== undefined) {
-var params = {
-    apiKey: 'trilogy',
-    title: name
-}
-omdbApi.get(params, function(err, data) {
-    if(err) throw err;
-    // console.log(data);
-    //title
-    console.log(data.Title);
-    //year
-    console.log(data.Year);
-    //rotten tomatoes ratings
-    console.log(data.Ratings[1].Source + ": " + data.Ratings[1].Value);
-    //country
-    console.log(data.Country);
-    // language of the movie
-    console.log(data.Language);
-    //plot
-    console.log(data.Plot);
-    //actors in the movie
-    console.log(data.Actors);
-});
-} else {
-    var params = {
-        apiKey: 'trilogy',
-        title: 'Mr. Nobody'
-    }
-    omdbApi.get(params, function(err, data) {
-        if(err) throw err;
-        // console.log(data);
-        //title
-        console.log(data.Title);
-        //year
-        console.log(data.Year);
-        //rotten tomatoes ratings
-        console.log(data.Ratings[1].Source + ": " + data.Ratings[1].Value);
-        //country
-        console.log(data.Country);
-        // language of the movie
-        console.log(data.Language);
-        //plot
-        console.log(data.Plot);
-        //actors in the movie
-        console.log(data.Actors);
-    });
-}
-        break;
-    case 'do-what-it-says':
-    fs.readFile('random.txt','utf8', function(error, data) {
-        if(error) throw error;
+// if command is do-what-it-says
+if (command == 'do-what-it-says') {
+    fs.readFile('random.txt', 'utf8', function (error, data) {
+        if (error) throw error;
         var a = data.split(',');
-        console.log(a);
+        var b = a[0];
+        var c = a[1];
+    //calling the function if the command is 'do-what-it-says'
+        choco(b,c);
     })
-        break;
+} else {
+    //function that take in everything except the do-what-liri-says
+    choco(command, name);
+}
+
+// function choco that will take in the commands and do its respective job
+function choco(command, name) {
+    switch (command) {
+        //tweeter portion
+        case 'my-tweets':
+            //using 'get' using the variable client that I have defined above
+            client.get('favorites/list', function (error, tweets, response) {
+                if (error) throw error;
+                console.log(tweets);  // The favorites. 
+                console.log(response);  // Raw response object. 
+            });
+            //using 'post'
+            // client.post('statuses/update', {status: name}, function(error, tweet, reponse) {
+            //     if(error) throw error;
+            //     console.log(tweet);
+            //     // console.log(response);
+            // })
+            break;
+
+        // spotify portion
+        case 'spotify-this-song':
+            //using variable spotify that i defined above
+            if (name !== undefined) {
+                spotify.search({ type: 'album', query: name }, function (err, data) {
+                    if (err) {
+                        console.log("there was an error somewhere with the spotify " + err);
+                    }
+                    var spot = data.albums.items;
+                    for (var i = 0; i < spot.length; i++) {
+                        var artists = spot[i].artists[0].name;
+                        var title = spot[i].name;
+                        var urlSpot = spot[i].external_urls.spotify;
+                        var album = spot[i].album_type;
+                        //showing the index along with the artists
+                        console.log(`${i + 1}: \n Artists: ${artists} \n Title: ${title} \n Preview Link: ${urlSpot} \n Album Type: ${album} \n`);
+                        console.log("===================================== \n");
+                    }
+                })
+            } else {
+                spotify.search({ type: 'album', query: 'Ace of Base' }, function (err, data) {
+                    if (err) {
+                        console.log("there was an error somewhere with the spotify " + err);
+                    }
+                    var spot = data.albums.items;
+                    for (var i = 0; i < spot.length; i++) {
+                        var artists = spot[i].artists[0].name;
+                        var title = spot[i].name;
+                        var urlSpot = spot[i].external_urls.spotify;
+                        var album = spot[i].album_type;
+                        //showing the index along with the artists
+                        console.log(`${i + 1}: \n Artists: ${artists} \n Title: ${title} \n Preview Link: ${urlSpot} \n Album Type: ${album} \n`);
+                        console.log("===================================== \n");
+                    }
+                })
+            }
+            break;
+
+        case 'movie-this':
+            if (name !== undefined) {
+                var params = {
+                    apiKey: 'trilogy',
+                    title: name
+                }
+                omdbApi.get(params, function (err, data) {
+                    if (err) throw err;
+                    // console.log(data);
+                    //title
+                    console.log(data.Title);
+                    //year
+                    console.log(data.Year);
+                    //rotten tomatoes ratings
+                    console.log(data.Ratings[1].Source + ": " + data.Ratings[1].Value);
+                    //country
+                    console.log(data.Country);
+                    // language of the movie
+                    console.log(data.Language);
+                    //plot
+                    console.log(data.Plot);
+                    //actors in the movie
+                    console.log(data.Actors);
+                });
+            } else {
+                var params = {
+                    apiKey: 'trilogy',
+                    title: 'Mr. Nobody'
+                }
+                omdbApi.get(params, function (err, data) {
+                    if (err) throw err;
+                    // console.log(data);
+                    //title
+                    console.log(data.Title);
+                    //year
+                    console.log(data.Year);
+                    //rotten tomatoes ratings
+                    console.log(data.Ratings[1].Source + ": " + data.Ratings[1].Value);
+                    //country
+                    console.log(data.Country);
+                    // language of the movie
+                    console.log(data.Language);
+                    //plot
+                    console.log(data.Plot);
+                    //actors in the movie
+                    console.log(data.Actors);
+                });
+            }
+            break;
+
+    }
 }
